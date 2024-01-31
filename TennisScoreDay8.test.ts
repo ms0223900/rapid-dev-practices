6c97dd7052c0bfd29e897315f26ace2c38595f4c
penguin-cho <penguin-cho@cloud-latitude.com>
2024-01-31 22:13:26 +0800

    refactor

--- TennisScoreDay8.test.ts
+++ TennisScoreDay8.test.ts
@@ -7,14 +7,27 @@ class Tennis {
         3: 'forty',
     };
     private _secondPlayerScore = 0;
+    private firstPlayerName: string;
+    private secondPlayerName: string;
+
+
+    constructor(firstPlayerName: string, secondPlayerName: string) {
+        this.firstPlayerName = firstPlayerName;
+        this.secondPlayerName = secondPlayerName;
+    }
 
     getScore() {
-        if (this._firstPlayerScore === this._secondPlayerScore) {
-            return `${this.scoreLookUp[this._secondPlayerScore]} all`;
+        if (this.isSameScore()) {
+            return this._firstPlayerScore >= 3 ? 'deuce' : `${this.scoreLookUp[this._secondPlayerScore]} all`;
         }
-        if (this._firstPlayerScore === 1 || this._firstPlayerScore === 2 || this._firstPlayerScore === 3) {
-            return `${this.scoreLookUp[this._firstPlayerScore]} ${this.scoreLookUp[this._secondPlayerScore]}`;
+
+        if (this.isMoreThan3()) {
+            const advPlayer = this._firstPlayerScore > this._secondPlayerScore ? this.firstPlayerName : this.secondPlayerName
+
+            return this.isWin() ? `${advPlayer} win` : `${advPlayer} adv`;
         }
+
+        return `${this.scoreLookUp[this._firstPlayerScore]} ${this.scoreLookUp[this._secondPlayerScore]}`;
     }
 
     firstPlayerScore() {
@@ -24,13 +37,29 @@ class Tennis {
     secondPlayerScore() {
         this._secondPlayerScore++
     }
+
+    private isSameScore() {
+        return this._firstPlayerScore === this._secondPlayerScore;
+    }
+
+    private isMoreThan3() {
+        return this._firstPlayerScore >= 3 && this._secondPlayerScore >= 3;
+    }
+
+    private isWin() {
+        return Math.abs(this._firstPlayerScore - this._secondPlayerScore) == 2;
+    }
+
+    private underOrEqual3() {
+        return this._firstPlayerScore === 0 || this._firstPlayerScore === 1 || this._firstPlayerScore === 2 || this._firstPlayerScore === 3;
+    }
 }
 
 describe('Tennis Score', function () {
     let tennis: Tennis
     beforeEach(() => {
 
-        tennis = new Tennis();
+        tennis = new Tennis('Tom', 'Pen');
     });
     it('should be love all', () => {
         expect(tennis.getScore()).toEqual('love all')
@@ -40,6 +69,12 @@ describe('Tennis Score', function () {
         tennis.firstPlayerScore()
         expect(tennis.getScore()).toEqual('fifteen love')
     });
+
+    it('should be love fifteen', () => {
+        tennis.secondPlayerScore()
+        expect(tennis.getScore()).toEqual('love fifteen')
+    });
+
 
     function firstPlayerScoreTimes(times: number) {
         for (let i = 0; i < times; i++) {
@@ -86,6 +121,39 @@ describe('Tennis Score', function () {
         firstPlayerScoreTimes(1);
         secondPlayerScoreTimes(1);
         expect(tennis.getScore()).toEqual('fifteen all')
+    });
+
+    it('should be thirty all', () => {
+        firstPlayerScoreTimes(2);
+        secondPlayerScoreTimes(2);
+        expect(tennis.getScore()).toEqual('thirty all')
+    });
+
+    it('should be deuce', () => {
+        firstPlayerScoreTimes(3);
+        secondPlayerScoreTimes(3);
+        expect(tennis.getScore()).toEqual('deuce')
+    });
+
+
+    it('should Tom adv', () => {
+        firstPlayerScoreTimes(4);
+        secondPlayerScoreTimes(3);
+        expect(tennis.getScore()).toEqual('Tom adv')
+    });
+
+
+    it('should Pen adv', () => {
+        firstPlayerScoreTimes(3);
+        secondPlayerScoreTimes(4);
+        expect(tennis.getScore()).toEqual('Pen adv')
+    });
+
+
+    it('should Tom win', () => {
+        firstPlayerScoreTimes(5);
+        secondPlayerScoreTimes(3);
+        expect(tennis.getScore()).toEqual('Tom win')
     });
 
 
