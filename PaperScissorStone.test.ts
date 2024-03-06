e2e6d0c3512cd9c2bcc0cb182a54377c67fcbd1b
penguin-cho <penguin-cho@cloud-latitude.com>
2024-03-06 21:30:56 +0800

    add more Tom win test cases

--- PaperScissorStone.test.ts
+++ PaperScissorStone.test.ts
@@ -1,9 +1,25 @@
 import {PaperScissorStone} from "./PaperScissorStone";
 
 describe('Paper Scissor Stone', function () {
-    it('should scissor win paper', () => {
+    it('should Tom win (scissor win paper)', () => {
         let paperScissorStone = new PaperScissorStone();
         paperScissorStone.firstPlayer('scissor')
+        paperScissorStone.secondPlayer('paper')
+        expect(paperScissorStone.getResult()).toEqual('Tom win')
+    });
+
+
+    it('should Tom win (paper win scissor)', () => {
+        let paperScissorStone = new PaperScissorStone();
+        paperScissorStone.firstPlayer('paper')
+        paperScissorStone.secondPlayer('scissor')
+        expect(paperScissorStone.getResult()).toEqual('Tom win')
+    });
+
+
+    it('should Tom win (stone win scissor)', () => {
+        let paperScissorStone = new PaperScissorStone();
+        paperScissorStone.firstPlayer('stone')
         paperScissorStone.secondPlayer('scissor')
         expect(paperScissorStone.getResult()).toEqual('Tom win')
     });
