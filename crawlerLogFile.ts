72a0981b8d3cc315f372889d29e28450172fb01b
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-10 19:44:46 +0800

    refactor: early continue

--- crawlerLogFile.ts
+++ crawlerLogFile.ts
@@ -7,14 +7,12 @@ var minOperations = function (logs = []) {
     let layers = 0;
     for (let i = 0; i < logs.length; i++) {
         const log = logs[i];
-        if (log === "../") {
-            layers = layers === 0 ? layers : layers - 1
-            continue
-        }
-        if (log === "./") {
-            continue
-        }
-        layers++
+        if (log === "./") continue
+
+        const toParent = log === "../";
+        layers = toParent ? (
+            layers === 0 ? layers : layers - 1
+        ) : layers + 1
     }
     return layers;
 };
