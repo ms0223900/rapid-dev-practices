0229478113cce056a4012d98ddce0b93084ffba4
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-11 14:50:36 +0800

    feat: stack empty

--- reverseParentheses.js
+++ reverseParentheses.js
@@ -17,7 +17,7 @@ var reverseParentheses = function (s = "(ed(et(oc))el)") {
         if (char === ")") {
             const popped = stack.pop();
             const reversed = reverseStr(popped);
-            stack[stack.length - 1] += reversed
+            stack[stack.length - 1] = (stack[stack.length - 1] || "") + reversed
             continue
         }
         stack[stack.length - 1] += char
