513c5b94c2437df9f273670e020d64b25723e8ef
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-19 11:31:28 +0800

    feat: split phase tdd

---
+++ refactor-practices/split-phase.test.ts
@@ -0,0 +1,28 @@
+interface Product {
+    discountRate: number;
+    discountThreshold: number;
+    basePrice: number;
+}
+
+interface ShippingMethod {
+    feePerCase: number;
+    discountedFee: number;
+    discountThreshold: number;
+}
+
+function priceOrder(product: Product, quantity: number, shippingMethod: ShippingMethod) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0)
+        * product.basePrice * product.discountRate;
+    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+        ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+    const shippingCost = quantity * shippingPerCase;
+    const price = basePrice - discount + shippingCost;
+    return price;
+}
+
+describe('price order', function () {
+    it('should get correct price', () => {
+        
+    });
+});
