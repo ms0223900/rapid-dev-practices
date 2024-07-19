34de27625fdc6f86ee511e86b69d512adb67b9b5
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-19 11:37:40 +0800

    test: complete first case

--- refactor-practices/split-phase.test.ts
+++ refactor-practices/split-phase.test.ts
@@ -23,6 +23,17 @@ function priceOrder(product: Product, quantity: number, shippingMethod: Shipping
 
 describe('price order', function () {
     it('should get correct price', () => {
-        
+        const actual = priceOrder({
+            discountRate: 0.1,
+            discountThreshold: 3,
+            basePrice: 1000,
+        }, 3, {
+            feePerCase: 10,
+            discountedFee: 10,
+            discountThreshold: 2
+        });
+        const expected = 3030;
+        expect(actual).toEqual(expected)
+
     });
 });
