57383c67e35651731fd70b92b8fb82f1e9aad1ac
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-19 17:48:53 +0800

    refactor: temp data with base price

--- refactor-practices/priceOrder.ts
+++ refactor-practices/priceOrder.ts
@@ -10,7 +10,9 @@ interface ShippingMethod {
     discountThreshold: number;
 }
 
-function applyShipping(basePrice: number, shippingMethod: ShippingMethod, quantity: number, discount: number, priceData: {}) {
+function applyShipping(shippingMethod: ShippingMethod, quantity: number, discount: number, {basePrice}: {
+    basePrice: number
+}) {
     const shippingCost = ((basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase) * quantity;
     return basePrice - discount + shippingCost;
@@ -20,6 +22,6 @@ export function priceOrder(product: Product, quantity: number, shippingMethod: S
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    let priceData = {};
-    return applyShipping(basePrice, shippingMethod, quantity, discount, priceData);
+    let priceData = {basePrice};
+    return applyShipping(shippingMethod, quantity, discount, priceData);
 }
