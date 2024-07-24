26d4aa377ee3db8dfbbd602d32444bf1f04870d6
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-24 20:10:25 +0800

    feat: compare usage

--- refactor-practices/encapsulate-data.test.ts
+++ refactor-practices/encapsulate-data.test.ts
@@ -25,16 +25,15 @@ class Organization {
     }
 }
 
+const organization = { name: "Acme Gooseberries", country: "GB" };
 describe('Encapsulate Data', function () {
     it('get organization data', () => {
-        const organization = { name: "Acme Gooseberries", country: "GB" };
         const organization1 = new Organization(organization);
         expect(organization1.name).toEqual("Acme Gooseberries")
         expect(organization1.country).toEqual("GB")
     });
 
     it('should set name', () => {
-        const organization = { name: "Acme Gooseberries", country: "GB" };
         const organization1 = new Organization(organization);
         organization1.name = "hi"
         expect(organization1.name).toEqual("hi")
@@ -42,12 +41,51 @@ describe('Encapsulate Data', function () {
     });
 
     it('should set country', () => {
-        const organization = { name: "Acme Gooseberries", country: "GB" };
         const organization1 = new Organization(organization);
         organization1.country = "CH"
         expect(organization1.country).toEqual("CH")
         expect(organization.country).toEqual("GB")
     });
+});
 
+const customerData = {
+    "1920": {
+        name: "martin",
+        id: "1920",
+        usages: {
+            "2016": {
+                "1": 50,
+                "2": 55,
+                // remaining months of the year
+            },
+            "2015": {
+                "1": 70,
+                "2": 63,
+                // remaining months of the year
+            }
+        }
+    },
+    "38673": {
+        name: "neal",
+        id: "38673",
+        // more customers in a similar form
+    }
+};
 
+function compareUsage(customerId: string, laterYear: string, month: string) {
+    const later = customerData[customerId].usages[laterYear][month];
+    const earlier = customerData[customerId].usages[String(Number(laterYear) - 1)][month];
+    return {
+        laterAmount: later,
+        change: later - earlier,
+    };
+}
+
+describe('Encapsulated customer data', function () {
+    it("compare usage", () => {
+        expect(compareUsage("1920", "2016", "2")).toEqual({
+            laterAmount: 55,
+            change: -8,
+        })
+    });
 });
