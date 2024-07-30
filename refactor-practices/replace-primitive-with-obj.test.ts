86c8947cde065903d94110e2682c9cf9c27759bc
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-30 20:58:01 +0800

    feat: get orders by price range

--- refactor-practices/replace-primitive-with-obj.test.ts
+++ refactor-practices/replace-primitive-with-obj.test.ts
@@ -23,8 +23,15 @@ class Priority {
 }
 
 class Order {
-    constructor(data: { priority: string }) {
+    constructor(data: { price: number; priority: string }) {
         this._priority = new Priority(data.priority)
+        this._price = data.price
+    }
+
+    private _price: number;
+
+    get price() {
+        return this._price;
     }
 
     private _priority: Priority;
@@ -42,15 +49,32 @@ function getHighPriorityOrdersCount(orders: Order[]) {
     return orders.filter(order => order.priority.higherThan(new Priority("normal"))).length;
 }
 
+function getOrdersByPriceRange(orders: Order[], priceRange: number[]) {
+    return orders.filter(order => order.price >= priceRange[0] && order.price <= priceRange[1]);
+}
+
 describe('Replace primitive with object', function () {
     it('should filter by priority', () => {
-        const order1 = new Order({priority: "normal"});
-        const order2 = new Order({priority: "high"});
-        const order3 = new Order({priority: "rush"});
+        const order1 = new Order({priority: "normal", price: 100});
+        const order2 = new Order({priority: "high", price: 200});
+        const order3 = new Order({priority: "rush", price: 300});
+
         const orders = [order1, order2, order3];
-        // const highPriorityCount = orders.filter(order => order.priorityString === "high" || order.priorityString === "rush").length;
         const highPriorityCount = getHighPriorityOrdersCount(orders);
 
         expect(highPriorityCount).toEqual(2)
     });
+
+    it('should get order by price range', () => {
+        const order1 = new Order({priority: "normal", price: 100});
+        const order2 = new Order({priority: "high", price: 200});
+        const order3 = new Order({priority: "rush", price: 300});
+
+        const orders = [order1, order2, order3];
+        const ordersByPriceRange = getOrdersByPriceRange(orders, [150, 250]);
+
+        expect(ordersByPriceRange.length).toEqual(1)
+    });
+
+
 });
