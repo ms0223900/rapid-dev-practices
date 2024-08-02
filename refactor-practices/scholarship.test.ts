e08085cc8a47166a8f9a3a76d7bf548ab8adea9c
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-02 21:18:36 +0800

    feat: get scholarship 1500 when scores average more than 90

--- refactor-practices/scholarship.test.ts
+++ refactor-practices/scholarship.test.ts
@@ -1,9 +1,26 @@
+function getAvg(scores: number[]) {
+    return scores.reduce((prev, next) => prev + next, 0) / scores.length;
+}
+
 function getScholarship(scores: number[]) {
+    if (getAvg(scores) >= 90) {
+        return 1500;
+    }
     return 1000;
 }
 
 describe('Scholarship', function () {
-    it('should get $1000 if courses scores average is 80.', () => {
+    it('should get $1000 if courses scores average more than 80.', () => {
         expect(getScholarship([80, 80])).toEqual(1000)
     });
+
+    it('should get $1000 if courses scores average more than 80.(avg is 81)', () => {
+        expect(getScholarship([80, 82])).toEqual(1000)
+    });
+
+    it('should get $1500 if courses scores average more than 90.', () => {
+        expect(getScholarship([80, 100])).toEqual(1500)
+    });
+
+
 });
