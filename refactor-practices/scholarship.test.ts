432267078bb70292d95125c8e1682b275196a6ee
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-03 09:42:26 +0800

    refactor: course as param

--- refactor-practices/scholarship.test.ts
+++ refactor-practices/scholarship.test.ts
@@ -45,6 +45,32 @@ class ScholarConfig {
     }
 }
 
+class Course {
+    private _subject: string;
+    private _score: number;
+
+    constructor(subject: string, score: number) {
+        this._subject = subject
+        this._score = score
+    }
+
+    static makeLiterature(score: number) {
+        return new Course("Literature", score);
+    }
+
+    static makeNature(score: number) {
+        return new Course("Nature", score);
+    }
+
+    static makeMath(score: number) {
+        return new Course("Math", score);
+    }
+
+    getScore() {
+        return this._score;
+    }
+}
+
 class ScholarshipCalculator {
     private _student: Student
     private _scholarConfig: ScholarConfig;
@@ -54,10 +80,15 @@ class ScholarshipCalculator {
         this._student = student
     }
 
-    calculate(scores: number[]) {
+    calculate(courses: Course[]) {
         const avgScoreScholarshipList = this._scholarConfig.getConfig(this._student);
+        return this.getScholar(avgScoreScholarshipList, courses);
+    }
+
+    private getScholar(avgScoreScholarshipList: AvgScoreScholarship[], courses: Course[]) {
         for (let i = 0; i < avgScoreScholarshipList.length; i++) {
             const avgScholar = avgScoreScholarshipList[i];
+            let scores = courses.map(course => course.getScore());
             if (this.checkShouldGetScholarship(scores, avgScholar)) {
                 return avgScholar.scholarship;
             }
@@ -83,43 +114,43 @@ describe('Normal students scholarship', function () {
     const scholarshipCalculator = new ScholarshipCalculator(new NormalStudent(), new ScholarConfig());
 
     it('should not get scholarship if less than 80.', () => {
-        expect(scholarshipCalculator.calculate([79, 80])).toEqual(0)
+        expect(scholarshipCalculator.calculate([new Course("Literature", 79), new Course("Nature", 80)])).toEqual(0)
     });
 
     it('should get $1000 if courses scores average more than 80.', () => {
-        expect(scholarshipCalculator.calculate([80, 80])).toEqual(1000)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(80), Course.makeNature(80)])).toEqual(1000)
     });
 
     it('should get $1000 if courses scores average more than 80.(avg is 81)', () => {
-        expect(scholarshipCalculator.calculate([80, 82])).toEqual(1000)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(80), Course.makeNature(82)])).toEqual(1000)
     });
 
     it('should get $1500 if courses scores average more than 90.', () => {
-        expect(scholarshipCalculator.calculate([80, 100])).toEqual(1500)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(80), Course.makeNature(100)])).toEqual(1500)
     });
 
     it('should get $1500 if courses scores average more than 90.(3 courses)', () => {
-        expect(scholarshipCalculator.calculate([80, 90, 100])).toEqual(1500)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(80), Course.makeNature(90), Course.makeMath(100)])).toEqual(1500)
     });
 
     it('should get $2000 if courses scores average more than 97.', () => {
-        expect(scholarshipCalculator.calculate([97, 98])).toEqual(2000)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(98), Course.makeNature(100)])).toEqual(2000)
     });
 
     it('should get $5000 if courses scores average 100.', () => {
-        expect(scholarshipCalculator.calculate([100, 100, 100])).toEqual(5000)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(100), Course.makeNature(100), Course.makeMath(100)])).toEqual(5000)
     });
 });
 describe('Disabled students scholarship', function () {
     const scholarshipCalculator = new ScholarshipCalculator(new DisabledStudent(), new ScholarConfig());
 
     it('should not get scholarship if less than 70.', () => {
-        expect(scholarshipCalculator.calculate([69, 70])).toEqual(0)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(69), Course.makeNature(70)])).toEqual(0)
     });
 
     it('should get scholarship $1000 if bigger or equal than 70.', () => {
-        expect(scholarshipCalculator.calculate([70, 70])).toEqual(1000)
+        expect(scholarshipCalculator.calculate([Course.makeLiterature(70), Course.makeNature(70)])).toEqual(1000)
     });
 
-
+// TODO, 如果有個規則是：數學班學生的國文分數超過 90 分，即給獎學金做鼓勵 ($500)
 });
