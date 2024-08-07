325f9715194da18fd27e4623f084c75a9218aa5a
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-07 20:51:33 +0800

    refactor: apply extracted top function

--- refactor-practices/topFunc.test.ts
+++ refactor-practices/topFunc.test.ts
@@ -1,11 +1,11 @@
 function top_calculateDistance(points = []) {
     let res = 0;
-    for (let i = 0; i < points.length; i++) {
+    for (let i = 1; i < points.length; i++) {
         res += getDistance(points[i - 1], points[i])
     }
     return res;
 
-    function getDistance(p1: any, p2: any) {
+    function getDistance(p1: Point, p2: Point) {
         const EARTH_RADIUS = 3959; // in miles
         const dLat = radians(p2.lat) - radians(p1.lat);
         const dLon = radians(p2.lon) - radians(p1.lon);
@@ -23,7 +23,12 @@ function top_calculateDistance(points = []) {
 
 }
 
-function trackSum(points = []) {
+interface Point {
+    lat: number
+    lon: number;
+}
+
+function trackSum(points: Point[] = []) {
     const time = calculateTime();
     const distance = calculateDistance();
     const pace = time / 60 / distance;
@@ -34,32 +39,25 @@ function trackSum(points = []) {
     });
 
     function calculateDistance() {
-        let res = 0;
-        for (let i = 0; i < points.length; i++) {
-            res += getDistance(points[i - 1], points[i])
-        }
-        return res;
-
-        function getDistance(p1: any, p2: any) {
-            const EARTH_RADIUS = 3959; // in miles
-            const dLat = radians(p2.lat) - radians(p1.lat);
-            const dLon = radians(p2.lon) - radians(p1.lon);
-            const a = Math.pow(Math.sin(dLat / 2), 2)
-                + Math.cos(radians(p2.lat))
-                * Math.cos(radians(p1.lat))
-                * Math.pow(Math.sin(dLon / 2), 2);
-            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            return EARTH_RADIUS * c;
-        }
-
-        function radians(deg: any) {
-            return deg * Math.PI / 180;
-        }
-
+        return top_calculateDistance(points);
     }
 
     function calculateTime() {
         return 0;
     }
-
 }
+
+describe('track summary', function () {
+    it('all ok', () => {
+        expect(trackSum([
+            {
+                lat: 10,
+                lon: 20,
+            },
+            {
+                lat: 20,
+                lon: 30,
+            }
+        ])).toEqual({"distance": 959.9270419268914, "pace": 0, "time": 0})
+    });
+});
