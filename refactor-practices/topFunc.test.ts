92e011934149ff5f028713290b1f540b2899d8b4
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-07 20:53:42 +0800

    refactor: align code

--- refactor-practices/topFunc.test.ts
+++ refactor-practices/topFunc.test.ts
@@ -1,33 +1,3 @@
-function getDistance(p1: Point, p2: Point) {
-    const EARTH_RADIUS = 3959; // in miles
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a = Math.pow(Math.sin(dLat / 2), 2)
-        + Math.cos(radians(p2.lat))
-        * Math.cos(radians(p1.lat))
-        * Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return EARTH_RADIUS * c;
-}
-
-function calculateDistance(points = []) {
-    let res = 0;
-    for (let i = 1; i < points.length; i++) {
-        res += getDistance(points[i - 1], points[i])
-    }
-    return res;
-
-}
-
-function radians(deg: any) {
-    return deg * Math.PI / 180;
-}
-
-interface Point {
-    lat: number
-    lon: number;
-}
-
 function trackSum(points: Point[] = []) {
     const time = calculateTime();
     const distance = calculateDistance(points);
@@ -57,3 +27,32 @@ describe('track summary', function () {
         ])).toEqual({"distance": 959.9270419268914, "pace": 0, "time": 0})
     });
 });
+
+function getDistance(p1: Point, p2: Point) {
+    const EARTH_RADIUS = 3959; // in miles
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p1.lon);
+    const a = Math.pow(Math.sin(dLat / 2), 2)
+        + Math.cos(radians(p2.lat))
+        * Math.cos(radians(p1.lat))
+        * Math.pow(Math.sin(dLon / 2), 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return EARTH_RADIUS * c;
+}
+
+function calculateDistance(points = []) {
+    let res = 0;
+    for (let i = 1; i < points.length; i++) {
+        res += getDistance(points[i - 1], points[i])
+    }
+    return res;
+}
+
+function radians(deg: any) {
+    return deg * Math.PI / 180;
+}
+
+interface Point {
+    lat: number
+    lon: number;
+}
