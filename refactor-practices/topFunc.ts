9d230508d05f04041b337b05b7d36743acb760df
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-07 20:37:43 +0800

    feat: copy a top calculateDistance function

--- refactor-practices/topFunc.ts
+++ refactor-practices/topFunc.ts
@@ -1,3 +1,28 @@
+function top_calculateDistance(points = []) {
+    let res = 0;
+    for (let i = 0; i < points.length; i++) {
+        res += getDistance(points[i - 1], points[i])
+    }
+    return res;
+
+    function getDistance(p1: any, p2: any) {
+        const EARTH_RADIUS = 3959; // in miles
+        const dLat = radians(p2.lat) - radians(p1.lat);
+        const dLon = radians(p2.lon) - radians(p1.lon);
+        const a = Math.pow(Math.sin(dLat / 2), 2)
+            + Math.cos(radians(p2.lat))
+            * Math.cos(radians(p1.lat))
+            * Math.pow(Math.sin(dLon / 2), 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return EARTH_RADIUS * c;
+    }
+
+    function radians(deg: any) {
+        return deg * Math.PI / 180;
+    }
+
+}
+
 function trackSum(points = []) {
     const time = calculateTime();
     const distance = calculateDistance();
@@ -14,13 +39,26 @@ function trackSum(points = []) {
             res += getDistance(points[i - 1], points[i])
         }
         return res;
+
+        function getDistance(p1: any, p2: any) {
+            const EARTH_RADIUS = 3959; // in miles
+            const dLat = radians(p2.lat) - radians(p1.lat);
+            const dLon = radians(p2.lon) - radians(p1.lon);
+            const a = Math.pow(Math.sin(dLat / 2), 2)
+                + Math.cos(radians(p2.lat))
+                * Math.cos(radians(p1.lat))
+                * Math.pow(Math.sin(dLon / 2), 2);
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            return EARTH_RADIUS * c;
+        }
+
+        function radians(deg: any) {
+            return deg * Math.PI / 180;
+        }
+
     }
 
     function calculateTime() {
-        return 0;
-    }
-
-    function getDistance(point: any, point2: any) {
         return 0;
     }
 
