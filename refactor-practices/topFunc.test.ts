617311059b45d3e72f7043f09c7b10a6baee0471
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-07 20:53:05 +0800

    refactor: extract functions to top

--- refactor-practices/topFunc.test.ts
+++ refactor-practices/topFunc.test.ts
@@ -1,3 +1,15 @@
+function getDistance(p1: Point, p2: Point) {
+    const EARTH_RADIUS = 3959; // in miles
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p1.lon);
+    const a = Math.pow(Math.sin(dLat / 2), 2)
+        + Math.cos(radians(p2.lat))
+        * Math.cos(radians(p1.lat))
+        * Math.pow(Math.sin(dLon / 2), 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return EARTH_RADIUS * c;
+}
+
 function calculateDistance(points = []) {
     let res = 0;
     for (let i = 1; i < points.length; i++) {
@@ -5,22 +17,10 @@ function calculateDistance(points = []) {
     }
     return res;
 
-    function getDistance(p1: Point, p2: Point) {
-        const EARTH_RADIUS = 3959; // in miles
-        const dLat = radians(p2.lat) - radians(p1.lat);
-        const dLon = radians(p2.lon) - radians(p1.lon);
-        const a = Math.pow(Math.sin(dLat / 2), 2)
-            + Math.cos(radians(p2.lat))
-            * Math.cos(radians(p1.lat))
-            * Math.pow(Math.sin(dLon / 2), 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return EARTH_RADIUS * c;
-    }
+}
 
-    function radians(deg: any) {
-        return deg * Math.PI / 180;
-    }
-
+function radians(deg: any) {
+    return deg * Math.PI / 180;
 }
 
 interface Point {
