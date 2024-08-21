1418f510744cd4a8dfe369335b49c10b527b42df
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-21 14:09:00 +0800

    test: renderPhoto test

---
+++ refactor-practices/statementIntoFn.test.ts
@@ -0,0 +1,53 @@
+interface Photo {
+    location: string;
+    date: Date;
+    title: string;
+}
+
+interface Person {
+    name: string;
+    photo: Photo;
+}
+
+function emitPhotoData(aPhoto: Photo) {
+    const result = [];
+    result.push(`<p>location: ${aPhoto.location}</p>`);
+    result.push(`<p>date: ${aPhoto.date.toDateString()}</p>`);
+    return result.join("\n");
+}
+
+function renderPhoto(photo: Photo) {
+    return photoDiv(photo);
+}
+
+function renderPerson(outStream: any, person: Person) {
+    const result = [];
+    result.push(`<p>${person.name}</p>`);
+    result.push(renderPhoto(person.photo));
+    result.push(`<p>title: ${person.photo.title}</p>`);
+    result.push(emitPhotoData(person.photo));
+    return result.join("\n");
+}
+
+function photoDiv(p: Photo) {
+    return [
+        "<div>",
+        `<p>title: ${p.title}</p>`,
+        emitPhotoData(p),
+        "</div>",
+    ].join("\n");
+}
+
+describe('render test', function () {
+    it('renderPhoto', () => {
+        expect(renderPhoto({
+            location: 'test',
+            date: new Date("2024-01-01"),
+            title: 'test'
+        })).toBe(`<div>
+<p>title: test</p>
+<p>location: test</p>
+<p>date: Mon Jan 01 2024</p>
+</div>`);
+    });
+});
