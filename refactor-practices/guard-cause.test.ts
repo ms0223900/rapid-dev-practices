65898f271d94599798fbe6f5952967d1d60004ba
penguin-cho <penguin-cho@cloud-latitude.com>
2024-10-18 12:22:34 +0800

    refactor: remove temp var

--- refactor-practices/guard-cause.test.ts
+++ refactor-practices/guard-cause.test.ts
@@ -7,11 +7,9 @@ interface Instrument {
 }
 
 function adjustedCapital(anInstrument: Instrument): number {
-    let result = 0;
     if (anInstrument.capital <= 0) return 0;
     if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return 0;
-    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-    return result;
+    return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
 
 
