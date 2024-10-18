c1f57c8f6017eac98c11fa59bbb2750fae7fed50
penguin-cho <penguin-cho@cloud-latitude.com>
2024-10-18 12:20:39 +0800

    refactor: check capital early return

--- refactor-practices/guard-cause.test.ts
+++ refactor-practices/guard-cause.test.ts
@@ -8,10 +8,9 @@ interface Instrument {
 
 function adjustedCapital(anInstrument: Instrument): number {
     let result = 0;
-    if (anInstrument.capital > 0) {
-        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-        }
+    if (anInstrument.capital <= 0) return 0;
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+        result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
     }
     return result;
 }
