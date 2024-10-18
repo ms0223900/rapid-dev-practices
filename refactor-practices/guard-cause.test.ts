a753190eee984d0d7baf99ba4d73637891e8dc9e
penguin-cho <penguin-cho@cloud-latitude.com>
2024-10-18 12:23:53 +0800

    refactor: much simpler with one clause

--- refactor-practices/guard-cause.test.ts
+++ refactor-practices/guard-cause.test.ts
@@ -7,8 +7,11 @@ interface Instrument {
 }
 
 function adjustedCapital(anInstrument: Instrument): number {
-    if (anInstrument.capital <= 0) return 0;
-    if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return 0;
+    if (
+        anInstrument.capital <= 0 ||
+        anInstrument.interestRate <= 0 ||
+        anInstrument.duration <= 0
+    ) return 0;
     return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
 
