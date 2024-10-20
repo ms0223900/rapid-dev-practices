5675bb15430004226c86ec72f43e0f8b5469c7fc
penguin-cho <penguin-cho@cloud-latitude.com>
2024-10-20 17:36:54 +0800

    refactor: extract getHistoryLengthFactor

--- refactor-practices/polymorph2.test.ts
+++ refactor-practices/polymorph2.test.ts
@@ -30,10 +30,13 @@ class Rating {
     }
 
     getVoyageAndHistoryLengthFactor() {
-        let result = 0;
-        if (this.history.length > 8) result += 1;
+        let result = this.getHistoryLengthFactor();
         if (this.voyage.length > 14) result -= 1;
         return result;
+    }
+
+    getHistoryLengthFactor() {
+        return this.history.length > 8 ? 1 : 0;
     }
 
     get voyageRisk() {
