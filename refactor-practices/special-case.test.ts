d3f360a847921fa2866584aab53bcb6ad9f07bde
penguin-cho <penguin-cho@cloud-latitude.com>
2024-11-06 19:51:08 +0800

    feat: site class get customer

--- refactor-practices/special-case.test.ts
+++ refactor-practices/special-case.test.ts
@@ -10,18 +10,45 @@ class SpecialCaseCustomer {
     }
 }
 
-function isUnknown(arg: SpecialCaseCustomer | string) {
-    if (!((arg instanceof SpecialCaseCustomer) || (arg === "unknown")))
+class UnknownCustomer {
+    get name() {
+        return "unknown";
+    }
+}
+
+class Site {
+    private _customer: SpecialCaseCustomer | UnknownCustomer;
+
+    constructor(_customer: SpecialCaseCustomer | UnknownCustomer) {
+        this._customer = _customer;
+    }
+
+    get customer() {
+        if (this._customer.name === "unknown") {
+            return new UnknownCustomer();
+        }
+        return this._customer;
+    }
+}
+
+function isUnknown(arg: SpecialCaseCustomer | UnknownCustomer) {
+    if (!((arg instanceof SpecialCaseCustomer) || (arg instanceof UnknownCustomer)))
         throw new Error(`investigate bad value: <${arg}>`);
     return ((arg as SpecialCaseCustomer).name === "unknown");
 }
 
 describe('special case', () => {
-    it('should return true if the argument is "unknown"', () => {
-        expect(isUnknown(new SpecialCaseCustomer("unknown"))).toBe(true);
+    it('should return true if the customer is "unknown"', () => {
+        const site = new Site(new SpecialCaseCustomer("unknown"));
+        expect(isUnknown(site.customer)).toBe(true);
     });
 
-    it('should throw an error if the argument is not a SpecialCaseCustomer or "unknown"', () => {
-        expect(() => isUnknown("abc")).toThrow();
+    it('should return false if the customer is not "unknown"', () => {
+        const site = new Site(new SpecialCaseCustomer("known"));
+        expect(isUnknown(site.customer)).toBe(false);
+    });
+
+    it('should throw an error if the customer is not a SpecialCaseCustomer or UnknownCustomer', () => {
+        expect(() => isUnknown("abc" as any)).toThrow();
     });
 });
