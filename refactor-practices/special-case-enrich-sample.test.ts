114df26ffb3b6d8247cbd89a1e8d05c214565035
penguin-cho <penguin-cho@cloud-latitude.com>
2024-11-16 09:23:14 +0800

    feat: getPlan and getWeeksDelinquent

--- refactor-practices/special-case-enrich-sample.test.ts
+++ refactor-practices/special-case-enrich-sample.test.ts
@@ -12,6 +12,12 @@ interface SiteInEnrichCase {
     customer: string | CustomerInEnrichCase;
 }
 
+const registry = {
+    billingPlans: {
+        basic: "basic-plan-451"
+    }
+}
+
 const site1: SiteInEnrichCase = {
     name: "Acme Boston",
     location: "Malden MA",
@@ -42,9 +48,38 @@ function getCustomerName(site: SiteInEnrichCase) {
     return customer.name;
 }
 
+function getPlanForCustomer(site: SiteInEnrichCase) {
+    const customer = site.customer;
+    if (isUnknown(customer)) {
+        return registry.billingPlans.basic;
+    }
+    return customer.billingPlan;
+}
+
+
+function getWeeksDelinquentForCustomer(site: SiteInEnrichCase) {
+    const customer = site.customer;
+    if (isUnknown(customer)) {
+        return 0;
+    }
+    return customer.paymentHistory.weeksDelinquentInLastYear;
+}
+
+
+
 describe("getCustomerName", () => {
     it("should return the name of the customer", () => {
         expect(getCustomerName(site1)).toBe("Acme Industries");
         expect(getCustomerName(site2)).toBe("occupant");
+    });
+
+    it("should return the plan for the customer", () => {
+        expect(getPlanForCustomer(site1)).toBe("plan-451");
+        expect(getPlanForCustomer(site2)).toBe("basic-plan-451");
+    });
+
+    it("should return the weeks delinquent for the customer", () => {
+        expect(getWeeksDelinquentForCustomer(site1)).toBe(7);
+        expect(getWeeksDelinquentForCustomer(site2)).toBe(0);
     });
 });
