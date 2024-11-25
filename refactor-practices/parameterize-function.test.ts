f37bcc3ed9bc051b51b45b2f4870bcdad7148788
penguin-cho <penguin-cho@cloud-latitude.com>
2024-11-25 10:10:52 +0800

    refactor: apply all withinBand

--- refactor-practices/parameterize-function.test.ts
+++ refactor-practices/parameterize-function.test.ts
@@ -1,22 +1,10 @@
 function baseCharge(usage: number) {
     if (usage < 0) return usd(0);
     const amount =
-        bottomBand(usage) * 0.03
+        withinBand(usage, 0, 100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
-        + topBand(usage) * 0.07;
+        + withinBand(usage, 200, Infinity) * 0.07;
     return usd(amount);
-}
-
-function bottomBand(usage: number) {
-    return Math.min(usage, 100);
-}
-
-function middleBand(usage: number) {
-    return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage: number) {
-    return usage > 200 ? usage - 200 : 0;
 }
 
 function withinBand(usage: number, bottom: number, top: number) {
